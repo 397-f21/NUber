e20b3a865681c54a7b7ebe90039606b18060840c
Brian Tang <btang@blt-macbook.lan>
2021-10-22 14:22:12 -0500

    changed display to dynamic dependent on time entered

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const PeopleList = ({ time, timeList }) => {
+  return (
+    timeList[time] ? timeList[time].map((name, key) => <li key={key}>{name}</li>) : null
+  );
+}
+
 const App = () => {
-  const peopleList = {
+  const timeList = {
     "00:00": [],
     "01:00": [],
     "02:00": [],
@@ -13,7 +19,7 @@ const App = () => {
     "07:00": [],
     "08:00": [],
     "09:00": [],
-    "10:00": [],
+    "10:00": ["Billy", "Bob", "Joe"],
     "11:00": [],
     "12:00": [],
     "13:00": [],
@@ -31,21 +37,21 @@ const App = () => {
 
   const [time, setTime] = useState(new Date());
 
-  const changeHandler = () => {
-
+  const changeHandler = (e) => {
+    setTime(e.target.value);
   }
+
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>NUber</h1>
         <h2>Enter in your arrival time:</h2>
-        <input type="time" onChange={changeHandler} />
+        <input type="time" onChange={(e) => changeHandler(e)} />
         <h2>Potential Wildcats to Ride-Share:</h2>
         <ul>
-          <li>Sally</li>
-          <li>Bob</li>
-          <li>Derek</li>
+          <PeopleList time={time} timeList={timeList} />
         </ul>
       </header>
     </div>
