b13f22e8a4800db29b9043af85e747722a02d42e
Brian Tang <btang@blt-macbook.lan>
2021-10-22 13:58:27 -0500

    Webpage showing static results

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const peopleList = {
+    "00:00": [],
+    "01:00": [],
+    "02:00": [],
+    "03:00": [],
+    "04:00": [],
+    "05:00": [],
+    "06:00": [],
+    "07:00": [],
+    "08:00": [],
+    "09:00": [],
+    "10:00": [],
+    "11:00": [],
+    "12:00": [],
+    "13:00": [],
+    "14:00": [],
+    "15:00": [],
+    "16:00": [],
+    "17:00": [],
+    "18:00": [],
+    "19:00": [],
+    "20:00": [],
+    "21:00": [],
+    "22:00": [],
+    "23:00": []
+  }
+
+  const [time, setTime] = useState(new Date());
+
+  const changeHandler = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>NUber</h1>
+        <h2>Enter in your arrival time:</h2>
+        <input type="time" onChange={changeHandler} />
+        <h2>Potential Wildcats to Ride-Share:</h2>
+        <ul>
+          <li>Sally</li>
+          <li>Bob</li>
+          <li>Derek</li>
+        </ul>
       </header>
     </div>
   );
