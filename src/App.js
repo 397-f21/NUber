b20589f068354d821182d489171ee6c79e498ea5
Brian Tang <btang@blt-macbook.lan>
2021-10-29 11:17:44 -0500

    removed unused code and made output into a component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { useData } from './utilities/firebase.js';
-
-// const PeopleList = ({ time, timeList }) => {
-//   return (
-//     timeList[time] ? timeList[time].map((name, key) => <li key={key}>{name}</li>) : null
-//   );
-// }
-
-// const addScheduleTimes = ({schedule}) => ({
-//   students: mapValues(addCourseTimes, schedule.students)
-  
-// });
-
-// const mapValues = (fn, obj) => (
-//   Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
-// );
-
-// const addCourseTimes = course => ({
-//   ...course,
-//   //...timeParts(course.meets)
-// });
 
 const StudentList = ({ students, date, time }) => (
   <div>
@@ -35,34 +15,33 @@ const Student = ({ student }) => (
   </div>
 );
 
-
+const Results = ({ schedule, date, time }) => {
+  return (
+    <>
+      <h2>These Wildcats are looking for Ride-Share too!</h2>
+      <ul>
+        <StudentList students={ schedule["wildcats"] } date={ date } time={ time } />
+      </ul>
+    </>
+  );
+};
 
 const App = () => {
    const [time, setTime] = useState(new Date());
    const [date, setDate] = useState(new Date());
 
-  // useEffect(() => {
-  //   const dbRef = ref(database);
-  //   onValue(dbRef, (snapshot) => {
-  //     const poop = snapshot.val();
-  //     setDummy(poop);
-  //   });
-  // }, []);
-
   const changeTimeHandler = (e) => {
     setTime(e.target.value);
-    //console.log(time.getTime);
   }
 
   const changeDateHandler = (e) => {
     setDate(e.target.value);
-    //console.log(date);
   }
 
   const [schedule, loading, error] = useData('/');
   
   if (error) return <h1>{error}</h1>;
-  if (loading) return <h1>Loading the schedule...</h1>;
+  if (loading) return <h1>Loading the results...</h1>;
   
   return (
     <div className="App">
@@ -72,11 +51,7 @@ const App = () => {
         <input type="time" onChange={(e) => changeTimeHandler(e)} />
         
         <input type="date" onChange={(e) => changeDateHandler(e)} />
-        <h2>These Wildcats are looking for Ride-Share too!</h2>
-        
-        <ul>
-        <StudentList students = { schedule.wildcats } date = { date } time = { time } />
-        </ul>
+        <Results schedule={schedule} date={date} time={time} />
       </header>
     </div>
   );
