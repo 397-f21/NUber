7f3af237e45ffa82cbf0a84c46479e8fa976dd2b
Brian Tang <btang@blt-macbook.lan>
2021-11-02 21:04:16 -0500

    fixed test data format & finished 2nd test

--- src/__tests__/Results.test.js
+++ src/__tests__/Results.test.js
@@ -9,7 +9,7 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("result message", () => {
     it("renders no match msg", () => {
         const data = {
-            "/1635782400000": {
+            "1635782400000": {
                 "name": "Test",
                 "email": "test@gmail.com",
                 "netid": "test101",
@@ -24,16 +24,23 @@ describe("result message", () => {
 
     it("renders match message of one person", () => {
         const data = {
-            "/1635782400000": {
+            "1635782400000": {
                 "name": "Test",
                 "email": "test@gmail.com",
                 "netid": "test101",
                 "date": "2021-11-01",
                 "arrival": "11:00"
+            },
+            "/1635782700000": {
+                "name": "Test",
+                "email": "test@gmail.com",
+                "netid": "test101",
+                "date": "2021-11-01",
+                "arrival": "11:05"
             }
         };
 
-        const wrapper = shallow(<Results students={data} date={"2021-11-00"} time={"11:00"} />);
+        const wrapper = shallow(<Results students={data} date={"2021-11-01"} time={"11:10"} />);
         console.log("wrapper text:" + wrapper.text());
         expect(wrapper.text()).toMatch("These Wildcats are looking to rideshare too!");
     });
